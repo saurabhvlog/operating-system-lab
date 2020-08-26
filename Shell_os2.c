027ea908f30f42119fba07814a25ab3bf32255fc
Saurabh vishwkarma <60293006+saurabhvlog@users.noreply.github.com>
2020-08-26 15:38:21 +0530

    Add files via upload

---
+++ Shell_os2.c
@@ -0,0 +1,38 @@
+echo "enter the radius to calculate area and perimeter"
+
+ r=5
+
+p= `echo 3.14 \* 2 \*r | bc `
+a=` echo 3.14 \*r \* r |bc `
+
+
+echo "perimeter is $p and area is $a "
+
+
+
+
+echo "Enter two numbers to calculated GCD"
+  
+read a 
+read b
+   
+m = $a 
+  
+if [ $b -lt $m ] 
+then 
+m = $b 
+fi 
+  
+while [ $m -ne 0 ] 
+do
+x = `expr $a % $m` 
+y = `expr $b % $m` 
+  
+if [ $x -eq 0 -a $y -eq 0 ] 
+then 
+  
+break
+fi 
+m = `expr $m - 1` 
+  
+done 
